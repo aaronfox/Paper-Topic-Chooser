85b3b031c779a2951b343063e13d0d0b96f9fe36
aaronfox <aaron.m.fox3@gmail.com>
2018-11-06 05:43:16 -0500

    Now have a properly edited guitar_topics.txt (:

--- Paper-Chooser.py
+++ Paper-Chooser.py
@@ -17,9 +17,41 @@ print(text_file)
 for line in text_file:
     print(line)
     new_line = line.replace("§", "\n")
+    if new_line[0] == ' ' or new_line[0] == '-':
+        new_line = new_line[1:]
+    if new_line[0] == ' ' or new_line[0] == '-':
+        new_line = new_line[1:]
+    if new_line[0] == ' ' or new_line[0] == '-':
+        new_line = new_line[1:]
     print("new_line == " + new_line)
     write_file.write(new_line)
 text_file.close()
 write_file.close()
+
+# Eliminate empty lines
+read_file = open("edited_guitar_topics.txt", "r", encoding="utf8")
+write_file = open("./new_edited_guitar_topics.txt", "w", encoding="utf8")
+for line in read_file:
+    char_flag = False
+    index = 0
+    for char in line:
+        if char.isalpha():
+            char_flag = True
+        # Get rid of leading spaces
+        if char_flag == False:
+            if char == " ":
+                line = line[0:index] + line[index + 1:]
+        index = index + 1
+    if char_flag:
+        write_file.write(line)
+    if line[0] == " ":
+        print("line[0] == \" \" where line == " + line + " and line[0] == \"" + line[0] + "\"")
+
+
+read_file.close()
+write_file.close()
+
+
+
 
 print('hello!')
